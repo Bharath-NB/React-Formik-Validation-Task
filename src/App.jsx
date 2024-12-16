e0e6abfca0e15235c15dd9e2b82d9d9ebb730390
Bharath-NB <Bharath.s8499@gmail.com>
2024-12-16 18:55:59 +0530

    5th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import AdminDashboard from "./components/AdminDashboard";
-import "./App.css";
+// src/App.jsx
+import React from 'react';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <div className="app-container">
-      <AdminDashboard />
+    <div className="App">
+      <h1>Hello, World!</h1>
+      {/* Update image path */}
+      <img src="./public/bg.png" alt="Background" />
     </div>
   );
-};
+}
 
 export default App;
